2a071a383513d2578116b1d39212edf8e4615f35
Rachit Singh <rachit.singh.bbps.1@gmail.com>
2025-12-03 02:30:10 +0530

    fixed bug

--- backend/src/controllers/likeController.js
+++ backend/src/controllers/likeController.js
@@ -15,7 +15,6 @@ export const toggleLike = async (req, res) => {
                 }
             }
         })
-
         if (existingLike) {
             await prisma.like.delete({
                 where: {
